c237530f58c9fd14d7f6543de2ba02064015e9cd
juancastro000 <cdyutub@gmail.com>
2025-01-31 22:21:27 +0100

    createList test fail

--- src/main/java/dev/juancastro/list/list.java
+++ src/main/java/dev/juancastro/list/list.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class list {
 
-  
+    List<String> weekDays = new ArrayList<>();
+
 
 }
--- src/test/java/dev/juancastro/list/listTest.java
+++ src/test/java/dev/juancastro/list/listTest.java
@@ -1,13 +1,29 @@
 package dev.juancastro.list;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.ArrayList;
 
 public class listTest {
 
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    @DisplayName ("comprueba si se crea una lista con los 7 dias de la semana")
+    void testIfcreateDaysList_ReturnTrue() {
+        //given
+        list emptyList = new list();
+        //when
+        List<String> weekDays = emptyList.createList();
+        //then
+        assertThat(emptyList,instanceOf(list.class));
+        assertThat(weekDays, contains("monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"));
+        assertThat(weekDays, hasSize(7));
     }
+
+    
+
+
 }
